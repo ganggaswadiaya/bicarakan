8ab61cb10fa995084ef871d1233d5eae04bba782
ganggaswadiaya <ganggaswadiaya@gmail.com>
2022-09-14 18:40:44 +0800

    update width

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+window.addEventListener('scroll', function () {
+	Math.floor(this.document.documentElement.scrollTop) > 80 ? addNavBg() : removeNavBg();
+	activeLink();
+});
+
+const removeNavBg = () => document.querySelector('.navbar').classList.remove('bg-[#fbebf6e6]', 'backdrop-blur-lg', 'shadow-sm');
+const addNavBg = () => document.querySelector('.navbar').classList.add('bg-[#fbebf6e6]', 'backdrop-blur-lg', 'shadow-sm');
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+console.log(hamburgerMenu);
+hamburgerMenu.addEventListener('click', function () {
+	hamburgerMenu.classList.toggle('nav-menu');
+});
+
+const navItem = document.querySelectorAll('.nav-item');
+navItem.forEach((item) => {
+	item.addEventListener('click', function () {
+		hamburgerMenu.classList.remove('nav-menu');
+	});
+});
+
+const navLinkContents = document.querySelectorAll('.nav-link-content');
+function activeLink() {
+	let current = '';
+
+	navLinkContents.forEach((content) => {
+		const sectionTop = content.offsetTop;
+		if (scrollY >= sectionTop - 250) {
+			current = content.getAttribute('id');
+		}
+	});
+
+	const navLinkItems = document.querySelectorAll('.nav-item a');
+	navLinkItems.forEach((item) => {
+		item.classList.remove('active');
+		if (item.classList.contains(current)) {
+			item.classList.add('active');
+		}
+	});
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['index.html', './src/**/*.js'],
+	theme: {
+		fontFamily: {
+			poppins: '"Poppins", sans-serif',
+			inter: '"Inter", sans-serif',
+		},
+		extend: {
+			colors: {
+				body: '#FBEBEC',
+				btnColor: '#050914',
+			},
+			width: {
+				85: '350px',
+			},
+			scale: {
+				102: '1.02',
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms')],
+};
